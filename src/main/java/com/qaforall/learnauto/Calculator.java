01ee29f9e583730eb94639aba2c2ccaccde6ce08
timur r <cryptex.tim@gmail.com>
2024-10-11 19:12:31 -0400

    jira-01 Added two methods that return sum of two and three numbers

---
+++ src/main/java/com/qaforall/learnauto/Calculator.java
@@ -0,0 +1,15 @@
+package com.qaforall.learnauto;
+
+public class Calculator {
+
+	public int sumOfTwoNumbers(int numberOne, int numberTwo) {
+		int sum =numberOne + numberTwo;
+		return sum;
+	}
+	
+	public int sumOfThreeNumbers(int numberOne, int numberTwo, int numberThree) {
+		int sum =numberOne + numberTwo + numberThree;
+		return sum;
+	}
+
+}
