eadeca7235dbc8d15dba7e69f20b89b8ab6e0005
Jacob Babichaev <galidar333@gmail.com>
2024-10-11 19:01:45 -0400

    Jacob's changes have been made

---
+++ src/test/java/com/qaforall/learnauto/JacobClass.java
@@ -0,0 +1,11 @@
+package com.qaforall.learnauto;
+
+public class JacobClass {
+
+	public static void main(String[] args) {
+		System.out.println("Some stuff");
+		System.out.println("Learning Git");
+
+	}
+
+}
